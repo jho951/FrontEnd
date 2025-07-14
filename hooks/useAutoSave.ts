35f6e00121eb77c5bc2a6fa8a5ef3984b2b5d6d8
jhon S <jhons@jhonui-MacBookPro.local>
2025-07-14 19:42:16 +0900

    fix: 빌드 에러 수정

--- hooks/useAutoSave.ts
+++ hooks/useAutoSave.ts
@@ -1,16 +1,21 @@
 import { useEffect, useRef } from 'react';
 
 export function useAutosave<T>(value: T, delay: number, onSave: (value: T) => void) {
-  const timeoutRef = useRef<NodeJS.Timeout>();
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    timeoutRef.current && clearTimeout(timeoutRef.current);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
     timeoutRef.current = setTimeout(() => {
       onSave(value);
     }, delay);
 
     return () => {
-      clearTimeout(timeoutRef.current);
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
   }, [value, delay, onSave]);
 }

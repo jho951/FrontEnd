96687e9f39a5595ff7fb6cdc4935bab5b57852da
jhon S <jhons@jhonui-MacBookPro.local>
2025-07-25 01:16:56 +0900

    fix: adsense 삭제

--- components/layout/wrapper/HeaderWrapper.tsx
+++ components/layout/wrapper/HeaderWrapper.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useRef } from 'react';
 
 import Header from '@/components/layout/header/Header';
-import AdBanner from '@/components/common/banner/AdBanner';
 
 import { useElementHeight } from '@/hooks/useElementHeight';
-import { ADSENSE_SLOT_ID } from '@/constants';
 
 export default function HeaderWrapper() {
   const adRef = useRef<HTMLDivElement | null>(null);
@@ -16,7 +14,6 @@ export default function HeaderWrapper() {
 
   return (
     <>
-      <AdBanner ref={adRef} slotId={ADSENSE_SLOT_ID} />
       <Header />
     </>
   );
